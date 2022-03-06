8c4f1532f3a966b34a9888018ded4cfd4db16e3d
Metalkon <chrisc@bpgstudios.com>
2022-03-06 14:46:15 -0400

    v2

---
+++ Slots/Animation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slot_Machine
+{
+    internal class Animation1
+    {
+        static void Animation(string[] args)
+        {
+
+        }
+
+    }
+}
--- Slots/Program.cs
+++ Slots/Program.cs
@@ -2,23 +2,16 @@
 using System.Media;
 
 
-namespace Slot_machine
-
-{
+namespace Slot_Machine
+{ 
     class Program
-    {
-        static void Main(string[] args)
-        {
-            int num1 = 0;
-            int num2 = 0;
-            int num3 = 0;
-            int num4 = 0;
-            int num5 = 0;
-            int num6 = 0;
-            int num7 = 0;
-            int num8 = 0;
-            int num9 = 0;
 
+    {
+        static void Main()
+        {
+            int num1 = 0; int num2 = 0; int num3 = 0;
+            int num4 = 0; int num5 = 0; int num6 = 0;
+            int num7 = 0; int num8 = 0; int num9 = 0;
             SoundPlayer jackpot = new SoundPlayer(@"Music\Jackpot.wav");
             SoundPlayer kaching = new SoundPlayer(@"Music\Kaching.wav");
             SoundPlayer music = new SoundPlayer(@"Music\Music.wav");
@@ -29,20 +22,32 @@ namespace Slot_machine
             Console.WriteLine("Welcome to Metalkon's Shitty Slot Machine Game!");
             Console.WriteLine("-----------------------------------------------");
             Console.WriteLine("How much virtual cash would you like to start the game with? \n(Every spin will cost you $5 and you must match 3 numbers to win a prize)");
-            Console.Write("\nInsert Starting Amount: ");
+            Console.Write("\nInsert Starting Amount ($1000 Limit): ");
             string userResponse = (Console.ReadLine()); Console.Clear();
             while (!Int32.TryParse(userResponse, out _) || Convert.ToInt32(userResponse) < 0)
             {
-                Console.WriteLine("\nEnter ONLY numbers PLEASE!\n");
+                Console.WriteLine("Enter ONLY numbers PLEASE!");
+                Console.Write("Insert Starting Amount ($1000 Limit): ");
                 userResponse = Console.ReadLine();
+                Console.Clear();
+
             }
+            if (Convert.ToInt32(userResponse) > 1000)
+            {
+                Console.WriteLine("That's too much!");
+                Console.Write("Insert Starting Amount ($1000 Limit): ");
+                userResponse = Console.ReadLine();
+                Console.Clear();
+
+            }
+
             int cash = Convert.ToInt32(userResponse);
             Console.WriteLine($"[ {num1} ] - [ {num2} ] - [ {num3} ]\n[ {num4} ] - [ {num5} ] - [ {num6} ]\n[ {num7} ] - [ {num8} ] - [ {num9} ]");
             string response = null;
             Random RandomNumber = new Random();
             for (; ; )
             {
-                if (Convert.ToInt32(cash) <= 0)
+                if (Convert.ToInt32(cash) <= 4)
                 {
                     Console.WriteLine("You're now broke. GAME OVER");
                     Console.Read();
@@ -58,20 +63,21 @@ namespace Slot_machine
                 else
                 {
                     cash = cash - 5;
-                    num1 = RandomNumber.Next(1, 8);
-                    num2 = RandomNumber.Next(1, 8);
+                    num1 = RandomNumber.Next(1, 8); 
+                    num2 = RandomNumber.Next(1, 8); 
                     num3 = RandomNumber.Next(1, 8);
-                    num4 = RandomNumber.Next(1, 8);
-                    num5 = RandomNumber.Next(1, 8);
+                    num4 = RandomNumber.Next(1, 8); 
+                    num5 = RandomNumber.Next(1, 8); 
                     num6 = RandomNumber.Next(1, 8);
-                    num7 = RandomNumber.Next(1, 8);
-                    num8 = RandomNumber.Next(1, 8);
+                    num7 = RandomNumber.Next(1, 8); 
+                    num8 = RandomNumber.Next(1, 8); 
                     num9 = RandomNumber.Next(1, 8);
 
                     Console.Clear();
                     Console.WriteLine($"[ {num1} ] - [ {num2} ] - [ {num3} ]"); Console.Beep(); System.Threading.Thread.Sleep(350);
                     Console.WriteLine($"[ {num4} ] - [ {num5} ] - [ {num6} ]"); Console.Beep(); System.Threading.Thread.Sleep(350);
                     Console.WriteLine($"[ {num7} ] - [ {num8} ] - [ {num9} ]"); Console.Beep(); System.Threading.Thread.Sleep(350);
+                    // Note: Replace if with switch
                     if ((num1 == num2 && num2 == num3 && (num1 != 7)) || (num4 == num5 && num5 == num6 && (num4 != 7)) || (num7 == num8 && num8 == num9 && (num7 != 7)) || (num1 == num5 && num5 == num9 && (num7 != 7)) || (num3 == num5 && num5 == num7 && (num7 != 7)))
                     {
                         cash += 25;
